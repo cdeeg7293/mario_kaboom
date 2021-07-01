27a45358a88b14ae51382c013e8f137fb06cade5
cdeeg7293 <cdeeg1993@gmail.com>
2021-07-01 09:31:56 +0100

    added socre label and mario

--- game.js
+++ game.js
@@ -32,19 +32,49 @@ scene("game", () => {
         '                                               ',
         '                                               ',
         '                                               ',
-        '                                               ',
-        '                                               ',
-        '                                               ',
+        '    %     =*=%=                                     ',
+        '                     ^    ^   -+                   ',
+        '                              ()                  ',
         '================================    ===========',
     ]
 
     const levelCfg = {
         width: 20,
         height: 20,
-        '=': [sprite('block', solid())]
+        '=': [sprite('block'), solid()],
+        '$': [sprite('coin')],
+        '%': [sprite('surprise'), solid(), 'coin-surprise'],
+        '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
+        '}': [sprite('unboxed'), solid()],
+        '(': [sprite('pipe-bottom-left'), solid(), scale(0.5)],
+        ')': [sprite('pipe-bottom-right'), solid(), scale(0.5)],
+        '-': [sprite('pipe-top-left'), solid(), scale(0.5)],
+        '+': [sprite('pipe-top-right'), solid(), scale(0.5)],
+        '^': [sprite('evil-shroom'), solid()],
+        '#': [sprite('mushroom'), solid()],
     }
 
     const gameLevel = addLevel(map, levelCfg);
+
+    const scoreLabel = add([
+        text('score'),
+        pos(30, 6),
+        layer('ui'),
+        {
+            value: 'test',
+        }
+    ])
+
+    add([text('level ' + 'test', pos(4,6))])
+
+    const player = add([
+        sprite('mario'), solid(),
+        pos(30, 0),
+        body(),
+        origin('bot')
+    ])
+
+
 
 })
 
